849c3a669c59a1de0daaca45269a373b209c6b5a
Kacper <75948940+Sayukoo@users.noreply.github.com>
2025-12-28 22:42:18 +0100

    Update GoogleCalendarWidget.tsx

--- landing-page/components/GoogleCalendarWidget.tsx
+++ landing-page/components/GoogleCalendarWidget.tsx
@@ -1,15 +1,16 @@
 
 export default function GoogleCalendarWidget() {
   return (
-    <div className="w-full h-full min-h-[600px] overflow-hidden rounded-lg">
+    <div className="w-full rounded-lg">
       <iframe
         src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ1giqEn8r9GJbCeU8qHNnLb2djJcDieeG_apCipdUUoDuLT7vsigFUtaBfsCu5Gd07vgYRIY-Ip?gv=true"
         style={{ border: 0 }}
         width="100%"
-        height="600"
+        height="1600"
         frameBorder="0"
+        scrolling="no"
         title="Kalendarz rezerwacji wizyty"
-        className="w-full h-full"
+        className="w-full h-[1600px]"
       />
     </div>
   );
